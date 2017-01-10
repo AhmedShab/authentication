ed903391a4a19fab00020abe4cd52882c641f642
AhmedShab <ahmed.vuw@gmail.com>
2017-01-11 00:14:15 +1300

    setting up the DB connection

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 const router = require('./routes/router');
+
+mongoose.connect('mongodb://localhost:auth/auth');
 
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
@@ -12,4 +15,4 @@ router(app);
 
 app.listen(port, function(){
   console.log('Server listening on port:', port);
-})
+});
